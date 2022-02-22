7e5ebe993dd2eefbdab1346fcdf79a07dba27e6a
KharimMchatta <47448636+KharimMchatta@users.noreply.github.com>
2022-02-22 12:00:36 +0300

    Update press-enum.sh

--- press-enum.sh
+++ press-enum.sh
@@ -5,8 +5,7 @@ read -p 'enter the website you want to scan: ' url
 echo ''
 echo ''
 echo ''
-
-echo "----------user enumeration via rest api-------------------"
+echo "----------- User enumeration via rest API ---------------------"
 
 curl -s https://$url/wp-json/wp/v2/users | jq -r 'map({id: .id, name: .name, slug: .slug})'
 
@@ -14,13 +13,29 @@ curl -s https://$url/wp-json/wp/v2/users | jq -r 'map({id: .id, name: .name, slu
 echo ''
 echo ''
 echo ''
-echo "----------user enumeration via rest route----------------"
+echo "----------- User enumeration via rest route -------------------"
 
 curl -s https://$url/?rest_route=/wp/v2/users | jq -r 'map({id: .id, name: .name, slug: .slug})'
 
-echo ''
-echo ''
-echo ''
-echo "----------- sitemap xml user url ----------------------------"
 
-curl -s https://$url/author-sitemap.xml grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}' 
+echo ''
+echo ''
+echo ''
+echo "----------- User enumeration via author archive ---------------"
+
+
+
+echo ''
+echo ''
+echo ''
+echo "----------- User enumeration via feeds ------------------------"
+
+curl -s https://$url/feed/ | grep -Eo "(https)://[a-zA-Z0-9./?=_%:-]*" | grep "author" |tee -a 
+
+echo ''
+echo ''
+echo ''
+echo "----------- Sitemap xml user url ----------------------------"
+
+curl -s https://$url/author-sitemap.xml grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}'
+
