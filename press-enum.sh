dd74617792d57f68a21af5a0eccc5324c28d8099
KharimMchatta <47448636+KharimMchatta@users.noreply.github.com>
2022-02-24 13:42:27 +0300

    Update press-enum.sh

--- press-enum.sh
+++ press-enum.sh
@@ -29,7 +29,7 @@ echo ''
 echo ''
 echo "----------- User enumeration via rest API ---------------------"
 
-curl -s https://$url/wp-json/wp/v2/users | jq -r 'map({id: .id, name: .name, slug: .slug})'
+curl -s $url/wp-json/wp/v2/users | jq -r 'map({id: .id, name: .name, slug: .slug})'
 
 
 echo ''
@@ -37,7 +37,7 @@ echo ''
 echo ''
 echo "----------- User enumeration via rest route -------------------"
 
-curl -s https://$url/?rest_route=/wp/v2/users | jq -r 'map({id: .id, name: .name, slug: .slug})'
+curl -s $url/?rest_route=/wp/v2/users | jq -r 'map({id: .id, name: .name, slug: .slug})'
 
 
 echo ''
@@ -49,7 +49,7 @@ n=0
 
 while (($n<=50)) do
 
-curl -isL https://$url/?author=$n | grep "og:url" | grep -Eo "(https)://[a-zA-Z0-9./?=_%:-]*" | grep "author" | uniq
+curl -isL $url/?author=$n | grep "og:url" | grep -Eo "(https)://[a-zA-Z0-9./?=_%:-]*" | grep "author" | uniq
 
 ((n++))
 
@@ -60,11 +60,11 @@ echo ''
 echo ''
 echo "----------- User enumeration via feeds ------------------------"
 
-curl -s https://$url/feed/ | grep -Eo "(https)://[a-zA-Z0-9./?=_%:-]*" | grep "author" |tee -a 
+curl -s $url/feed/ | grep -Eo "(https)://[a-zA-Z0-9./?=_%:-]*" | grep "author" |tee -a 
 
 echo ''
 echo ''
 echo ''
 echo "----------- Sitemap xml user url ----------------------------"
 
-curl -s https://$url/author-sitemap.xml | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}'
+curl -s $url/author-sitemap.xml | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}'
